e9484a9cb964687ad958b0f0c7d63589270f7e58
Bilal Ahmed <55283639+Bilal05476@users.noreply.github.com>
2021-08-25 20:11:32 +0500

    navigation spy

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -14,17 +14,30 @@ const Navigation = () => {
     }
   };
 
-
   useEffect(() => {
     changeBackground();
     // adding the event when scroll change background
     window.addEventListener("scroll", changeBackground);
   });
+
+  //Add Active Menu Class in NavLinks
+  const activeLinks = (targetedClass) => {
+    // Add Blackish Color
+    const otherElements = document.querySelector(
+      ".navbar-nav a.menu-active-link"
+    );
+    otherElements.classList.remove("menu-active-link");
+
+    // Add Theme color
+    const element = document.querySelector(targetedClass);
+    element.classList.add("menu-active-link");
+  };
   return (
     <nav
       id="navbar"
-      className={`navbar navbar-expand-md navbar-light fixed-top ${
-        navbarBackground ? "scrollNavbar" : ""
+      className={`navbar navbar-expand-md navbar-light fixed-top 
+      ${navbarBackground ? "scrollNavbar" : ""} ${
+        navbarIcon ? "blurryBack" : ""
       }`}
     >
       <div className="container">
@@ -45,25 +58,53 @@ const Navigation = () => {
         </button>
         <div className="collapse navbar-collapse" id="navbarNavAlt">
           <div className="navbar-nav ml-auto mr-5">
-            <a className="nav-item nav-link active-link" href="#home">
+            <a
+              className="nav-item nav-link homeMenu menu-active-link"
+              onClick={() => activeLinks(".homeMenu")}
+              href="#home"
+            >
               Home
             </a>
-            <a className="nav-item nav-link" href="#about">
+            <a
+              className="nav-item nav-link aboutMenu"
+              onClick={() => activeLinks(".aboutMenu")}
+              href="#about"
+            >
               About
             </a>
-            <a className="nav-item nav-link" href="#experience">
+            <a
+              className="nav-item nav-link experienceMenu"
+              onClick={() => activeLinks(".experienceMenu")}
+              href="#experience"
+            >
               Experience
             </a>
-            <a className="nav-item nav-link" href="#portfolio">
+            <a
+              className="nav-item nav-link portfolioMenu"
+              onClick={() => activeLinks(".portfolioMenu")}
+              href="#portfolio"
+            >
               Portfolio
             </a>
-            <a className="nav-item nav-link" href="#services">
+            <a
+              className="nav-item nav-link servicesMenu"
+              onClick={() => activeLinks(".servicesMenu")}
+              href="#services"
+            >
               Services
             </a>
-            <a className="nav-item nav-link" href="#blog">
+            <a
+              className="nav-item nav-link blogMenu"
+              onClick={() => activeLinks(".blogMenu")}
+              href="#blog"
+            >
               Blog
             </a>
-            <a className="nav-item nav-link" href="#contact">
+            <a
+              className="nav-item nav-link contactMenu"
+              onClick={() => activeLinks(".contactMenu")}
+              href="#contact"
+            >
               Contact
             </a>
           </div>
